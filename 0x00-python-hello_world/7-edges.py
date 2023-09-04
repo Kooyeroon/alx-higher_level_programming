73f56beca6a93e926ac30cf57319d1dfbade6c2c
Kooyeroon <kooyeroon2013@gmail.com>
2023-09-04 03:01:09 -0700

    the fisrt task activities in python

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+the_first_3 = word[:3]
+the_last_2 = word[-2:]
+the_middle_word = word[1:-1]
+print("First 3 letters: {}".format(the_first_3))
+print("Last 2 letters: {}".format(the_last_2))
+print("Middle word: {}".format(the_middle_word))
